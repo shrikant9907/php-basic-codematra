0803f9208dd6fa130877088b09352b010990c711
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-30 20:14:05 +0530

    basic intro codematra

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include_once('header.php'); ?>
-
-
-
-<?php include_once('footer.php'); ?> 
+<section class="text-center">
+  <h1 class="mb-5">Welcome</h1>
+  <p>This is a sample project to learn PHP programming from the basic level.</p>
+  <p>Available for free download on <a class="btn btn-primary" href="https://codematra.com/">Code Matra</a></p>
+  <p>Designed and Developed By <a href="https://github.com/shrikant9907">Shrikant Yadav</a></p>
+</section>
+<?php include_once('footer.php');
