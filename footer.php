a806cac5e49c46a109af6b26d9a08e594e1bc84d
Shrikant Yadav <shrikant9907@gmail.com>
2021-09-30 20:13:07 +0530

    scripts added

--- footer.php
+++ footer.php
@@ -1,2 +1,7 @@
-</body>
+  <!-- Script Files --> 
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+
+  </body>
 </html> 
